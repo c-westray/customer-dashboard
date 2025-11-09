588ffebe894328d39ec82fae7d819193eedd9794
cellular-cello <cellawestray@comcast.net>
2025-11-09 09:07:33 -0600

    Implement pandas dataframe loading of json from bigquery api to server file transform_data.py

--- app/server/transform_data.py
+++ app/server/transform_data.py
@@ -5,6 +5,14 @@ import os
 from dotenv import load_dotenv
 from google.cloud import bigquery
 from google.oauth2 import service_account
+import pandas as pd
+import json
+from datetime import date, datetime
+
+def default_serializer(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()  # 'YYYY-MM-DD' or 'YYYY-MM-DDTHH:MM:SS'
+    raise TypeError(f"Type {type(obj)} not serializable")
 
 current_directory = os.path.dirname(__file__)
 project_root = os.path.join(current_directory, '..', '..')
@@ -35,28 +43,77 @@ print("Project ID:", project_id)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path_to_key
 
 
-# Instantiate BigQuery client
-if (path_to_key):
+# Instantiate BigQuery client 
+# if there is a file saved in the keys folder, connect using the key for local developement
+if os.path.isfile(path_to_key):
     print('Local development: Authenticating with BigQuery Json Key')
+    # Set up credentials using python bigquery client (from google.oauth2 import service_account)
     creds = service_account.Credentials.from_service_account_file(path_to_key)
     client = bigquery.Client(credentials=creds, project=project_id)
 else:
-    print('Production: Connecting to BigQuery Client')
+    print('Production: Connecting to BigQuery Client. For Google Cloud Run')
     client = bigquery.Client()
 
-'''
 
-# client = bigquery.Client()
+def write_to_file(filepath, filename, data):
+    path = os.path.join(filepath, filename)
+    try:
+        # Creates new file
+        with open(path, "x") as f:
+            #f.write(data)
+            json.dump(data, f, indent=4, default=default_serializer) #serializes dates properly for json
+            print("File created successfully.")
+    except FileExistsError:
+        print("Error: File already exists.")
+        inputstatus = True
+        while (inputstatus):
+            replace = input("Would you like to replace the file? (y/n) ").strip().lower()
+            if replace == 'y':
+                with open(path, "w") as f:
+                    # f.write(data)
+                    json.dump(data, f, indent=4, default=default_serializer) #serializes dates properly for json
+                    print("File created successfully.")
+                    inputstatus = False
+            elif replace == 'n':
+                print("Ok, ending program.")
+                inputstatus = False
+            else:
+                print("Invalid input. Try again")
 
-def fetch_data():
-    QUERY = """
-    SELECT *
-    FROM `klett-cx-analytics.license_staging_cleaned.step_zero_make_location_id`
-    LIMIT 100
-    """
-    # Execute query
-    rows = client.query(QUERY).result()
-    
-    # Return rows
-    return rows
- '''
+def fetch_license_data():
+        QUERY = (
+             'SELECT *'
+             'FROM `klett-cx-analytics.license_staging_cleaned.step_zero_make_location_id`'
+        )
+        query_job = client.query(QUERY) # API request. This line is BLOCKING. 
+        # NOT fully asynchronous like the promise-based js client. Returns a query_job object.
+        # Then you have to access query_job.result.
+        rows = query_job.result() # Waits for query to finish
+        df = query_job.to_dataframe()  # Converts rows to pandas DataFrame
+        data = df.to_dict(orient='records')  # List of dicts
+        return data
+        '''
+        ## Longer method:
+        rowdict = []
+        for row in rows:
+             rowdict.append(dict(row))
+        return rowdict
+        ## Alternate pythonic oneliner:
+        ## return [dict(row) for row in rows]
+        '''
+
+def process_license_data():
+    data = fetch_license_data()
+    print(data[0]['locationName']) # must access key with the exact type
+
+    #print(rowdict)
+    #Output to json file for local testing.
+    filepath = os.path.join(project_root, './app/server/server_test_data')
+    filename = 'testdata.json'
+    write_to_file(filepath, filename, data) 
+
+process_license_data()
+
+
+
+
